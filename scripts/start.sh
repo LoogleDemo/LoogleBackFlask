f335d23efae84f6f395a98e0fae09e1f3eac1e77
born2slayy77 <born2slayy77@gmail.com>
2024-08-03 03:39:16 +0900

    preparing for codedeploy

---
+++ scripts/start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ubuntu/flaskapp"
+APP_LOG="$PROJECT_ROOT/application.log"
+ERROR_LOG="$PROJECT_ROOT/error.log"
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# 애플리케이션 시작
+echo "$TIME_NOW > Flask 애플리케이션 시작" >> $DEPLOY_LOG
+nohup gunicorn -w 4 -b 0.0.0.0:8000 app:app > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f 'gunicorn')
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
