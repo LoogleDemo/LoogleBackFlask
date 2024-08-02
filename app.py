d8399b55adfe5cde31a144ce44da2df773917743
born2slayy77 <born2slayy77@gmail.com>
2024-08-03 04:53:56 +0900

    config changed

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import openai
 from openai.embeddings_utils import get_embedding, cosine_similarity
-from config import OPENAI_API_KEY, GEM_API_KEY
 import pandas as pd
 import numpy as np
 import os
 
 from flask import Flask, request, jsonify
-
 
 import google.generativeai as genai
 from IPython.display import Markdown
