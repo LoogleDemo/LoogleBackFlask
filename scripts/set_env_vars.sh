3b3ce2d78bf9126c68dd5a93a8ca07c5e727e77b
born2slayy <122500877+born2slayy@users.noreply.github.com>
2024-08-03 15:24:16 +0900

    Update set_env_vars.sh

--- scripts/set_env_vars.sh
+++ scripts/set_env_vars.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "export OPENAI_API_KEY=${OPENAI_API_KEY}" >> /etc/profile.d/my_env_vars.sh
 echo "export GEM_API_KEY=${GEM_API_KEY}" >> /etc/profile.d/my_env_vars.sh
+source /etc/profile.d/my_env_vars.sh
